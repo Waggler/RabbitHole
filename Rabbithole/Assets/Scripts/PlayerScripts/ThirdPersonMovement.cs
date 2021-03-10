fef940c6644c133bf02f22429393f69ef2df7d20
Waggler <khdavis4@gmail.com>
2021-03-09 21:37:13 -0500

    ThirdPersonFunctionality
    Added Camera and Movement

---
+++ Rabbithole/Assets/Scripts/PlayerScripts/ThirdPersonMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    [Header("Camera Settings")]
+    public CharacterController controller;
+    public Transform cam;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    [Header("Movement Settings")]
+    public float speed = 6f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+    bool isGrounded;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        // Gets input from player movement
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        // Makes sures Player can move in multiple directions
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        // Checks if player is grounded
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // Handles Gravity
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }// END Update
+}
