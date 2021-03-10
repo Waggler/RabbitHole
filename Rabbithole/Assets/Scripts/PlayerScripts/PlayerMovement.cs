a6e7b2b485d9acf64a2b0df602ae8fb3e0cabe61
Waggler <khdavis4@gmail.com>
2021-03-10 17:46:37 -0500

    Testing

--- Rabbithole/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Rabbithole/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -2,39 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
 	private CharacterController characterController;
 	private Animator animator;
 
 	[SerializeField]
-	private float moveSpeed = 100;
+	private float moveSpeed;
 	[SerializeField]
 	private float turnSpeed = 5f;
 
 	private void Awake()
 	{
 		characterController = GetComponent<CharacterController>();
-		//animator = GetComponentInChildren<Animator>();
+		//1animator = GetComponentInChildren<Animator>();
 	}
 
-	private void Update()
+
+    private void Start()
+    {
+		Init();
+    }
+
+
+	public void Init()
+    {
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+
+    private void Update()
 	{
 		var horizontal = Input.GetAxis("Horizontal");
 		var vertical = Input.GetAxis("Vertical");
 
 		var movement = new Vector3(horizontal, 0, vertical);
 
-		characterController.SimpleMove(movement * Time.deltaTime * moveSpeed);
-
+		//characterController.SimpleMove(movement * Time.deltaTime * moveSpeed);
+		characterController.Move(movement * Time.deltaTime * moveSpeed);
 		//animator.SetFloat("Speed", movement.magnitude);
 
+		
+		
 		if (movement.magnitude > 0)
 		{
-			Quaternion newDirection = Quaternion.LookRotation(movement);
+			//Quaternion newDirection = Quaternion.LookRotation(movement);
+
+			Quaternion newDirection = transform.localRotation;
 
 			transform.rotation = Quaternion.Slerp(transform.rotation, newDirection, Time.deltaTime * turnSpeed);
 		}
+		
 	}
 }
 
