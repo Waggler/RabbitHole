997ddbfc8c445fe3c7646446553c22686bbecae7
Wi024933 <beattywf@knights.ucf.edu>
2021-03-17 13:40:08 -0400

    Dialogue

---
+++ Rabbithole/Assets/Scripts/UI Scripts/DialogueManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    [Header ("Dialogue Elements")]
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+    public Animator animator;
+    
+    
+    Queue<string> sentences;
+
+
+    // Start is called before the first frame
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    private void Update()
+    {
+
+        // Press Spacebar to go to the next sentence
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            DisplayNextSentence();
+        }
+    }
+
+    // Starts dialogue
+    public void StartDialogue(Dialogue dialogue)
+    {
+
+        // Open the dialogue box visually
+        animator.SetBool("isOpen", true);
+
+        // Checks who's talking
+        nameText.text = dialogue.name;
+
+        // Gets rid of old convo
+        sentences.Clear();
+
+        // Gets sentences ready
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+
+    }
+
+    public void DisplayNextSentence()
+    {
+        // Stops chatting when you run out of things to say
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    void EndDialogue()
+    {
+        animator.SetBool("isOpen", false);
+    }
+
+}
---
+++ Rabbithole/Assets/Scripts/UI Scripts/DialogueTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    // Gets Dialogue
+    public Dialogue dialogue;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        TriggerDialogue();
+    }
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+}
