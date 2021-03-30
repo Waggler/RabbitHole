8cc8d4fe052528d135b84b1667102bc2e1d50d61
Wi024933 <beattywf@knights.ucf.edu>
2021-03-30 14:47:35 -0400

    Hub, Level, and Menu Updates

---
+++ Rabbithole/Assets/Scripts/NextLevelScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevelScript : MonoBehaviour
+{
+    public GameObject beachTrigger;
+    public GameObject burrowTrigger;
+    public GameObject mountainTrigger;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Burrow");
+        }
+    }
+
+
+
+    public void Burrow()
+    {
+        SceneManager.LoadScene("Burrow");
+        Debug.Log("Load Burrow level");
+    }
+
+    public void Mountain()
+    {
+        SceneManager.LoadScene("Mountain");
+        Debug.Log("Load Mountain level");
+    }
+
+    public void Beach()
+    {
+        SceneManager.LoadScene("Beach");
+        Debug.Log("Load Beach level");
+    }
+}
--- Rabbithole/Assets/Scripts/UI Scripts/MenuScript.cs
+++ Rabbithole/Assets/Scripts/UI Scripts/MenuScript.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.EventSystems;
 
 public class MenuScript : MonoBehaviour
 {
