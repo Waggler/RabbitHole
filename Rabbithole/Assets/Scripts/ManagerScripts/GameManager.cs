53107a1740e02af6952a153baf8ac6086dc47ba0
allusion5 <46662003+allusion5@users.noreply.github.com>
2021-04-04 14:08:12 -0400

    SceneSetter in GM

--- Rabbithole/Assets/Scripts/ManagerScripts/GameManager.cs
+++ Rabbithole/Assets/Scripts/ManagerScripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,6 +11,19 @@ public class GameManager : MonoBehaviour
     public GameObject player;
     private int maxHealth = 8;
     public int health;
+
+    [Header("Level Completion")]
+    public bool tutComplete = false;
+    public bool level1Complete = false;
+    public bool level2Complete = false;
+    public bool level3Complete = false;
+
+    [Header("Scene Transitions")]
+    public string currentScene;
+    public string targetScene;
+
+    [Header("Pause Menu")]
+    public bool isPaused;
 
     void Awake()
     {
@@ -27,5 +41,17 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         health = maxHealth;
+        SceneID();
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadSceneAsync(targetScene);
+        SceneID();
+    }
+
+    public void SceneID()
+    {
+        currentScene = SceneManager.GetActiveScene().ToString();
     }
 }
