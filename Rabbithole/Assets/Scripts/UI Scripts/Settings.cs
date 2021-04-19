d0409841ce835b099f93461b01180935ac040625
Wi024933 <beattywf@knights.ucf.edu>
2021-04-19 15:33:35 -0400

    quick update, back hort

--- Rabbithole/Assets/Scripts/UI Scripts/Settings.cs
+++ Rabbithole/Assets/Scripts/UI Scripts/Settings.cs
@@ -5,13 +5,39 @@ using UnityEngine.Audio;
 public class Settings : MonoBehaviour
 {
     public AudioMixer audioMixer;
+   
+    public bool bClip;
+    public bool invincible;
     public bool gameisPaused = false;
     public void SetVolume (float volume)
     {
         audioMixer.SetFloat("volume",volume);
     }
-    void Update()
+
+    // Bottomless Clip bool
+    public void BClip()
     {
-        
+        if (bClip == false)
+        {
+            bClip = true;
+        } else if(bClip == true)
+        {
+            bClip = false;
+        }
     }
+
+    // Invincibility Bool
+
+    public void Invincibility()
+    {
+        if (invincible == false)
+        {
+            invincible = true;
+        }
+        else if (invincible == true)
+        {
+            invincible = false;
+        }
+    }
+
 }
